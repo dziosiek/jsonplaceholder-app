84dec53c5e118e088c12bdefd78bf532719084ff
awlosek <awlosek@leroymerlin.pl>
2024-03-16 15:01:42 +0100

    add project scaffold

---
+++ src/main/java/com/example/jsonplaceholderapp/JsonplaceholderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonplaceholderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonplaceholderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonplaceholderAppApplication.class, args);
+	}
+
+}
